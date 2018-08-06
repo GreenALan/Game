e5e49691b5c62563274db28642bfb5a5aa18e05e
吕绍辉 <alen.lv@bytedance.com>
2018-08-06 21:05:42 +0800

    1

---
+++ include/BaseInclude.h
@@ -0,0 +1,17 @@
+//
+//  BaseInclude.h
+//  OpenGL
+//
+//  Created by lvshaohui on 2018/8/2.
+//  Copyright © 2018年 lvshaohui. All rights reserved.
+//
+
+#ifndef BaseInclude_h
+#define BaseInclude_h
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#endif /* BaseInclude_h */
